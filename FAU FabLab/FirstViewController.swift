cb886a48d84d9e5b78805541af4044bd1958ec78
Max Jalowski <max.jalowski@fau.de>
2015-07-09 21:08:31 +0200

    Initial Commit

---
+++ FAU FabLab/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FAU FabLab
+//
+//  Created by Max Jalowski on 09.07.15.
+//  Copyright (c) 2015 FAU MAD FabLab. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FAU FabLabTests/FAU_FabLabTests.swift
@@ -0,0 +1,36 @@
+//
+//  FAU_FabLabTests.swift
+//  FAU FabLabTests
+//
+//  Created by Max Jalowski on 09.07.15.
+//  Copyright (c) 2015 FAU MAD FabLab. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FAU_FabLabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
