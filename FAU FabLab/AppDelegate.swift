07a0b0eb2987548fab6f312445eecc9c264cbe6a
Sebastian Haubner <sebastian.haubner@fau.de>
2015-07-26 15:00:33 +0200

    remove dummy json data

--- FAU FabLab/AppDelegate.swift
+++ FAU FabLab/AppDelegate.swift
@@ -16,15 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
-        if let file = NSBundle(forClass:AppDelegate.self).pathForResource("test", ofType: "json") {
-            let data = NSData(contentsOfFile: file)!
-            let json = JSON(data:data)
-            println(json)
-        } else {
-            println(JSON.nullJSON)
-        }
-        
         return true
     }
 
