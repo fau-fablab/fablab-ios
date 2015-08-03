7e1b04571ea24ef1f006ceea5a4da68923ec9da0
Emanuel Eimer <mail@eeimer.de>
2015-08-03 11:43:57 +0200

    Added Scanner for Checkout, did some refactoring

---
+++ FAU FabLab/Controllers/BarcodeScanner/CheckoutCodeScannerViewController.swift
@@ -0,0 +1,47 @@
+import UIKit
+import AVFoundation
+import RSBarcodes
+
+class CheckoutCodeScannerViewController: RSCodeReaderViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.focusMarkLayer.strokeColor = UIColor.greenColor().CGColor
+        self.cornersLayer.strokeColor = UIColor.greenColor().CGColor
+        
+        self.barcodesHandler = { barcodes in
+            self.session.stopRunning()
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                let checkoutCode = barcodes[0].stringValue
+                
+                
+                self.dismissViewControllerAnimated(true, completion:nil )
+                
+                NSNotificationCenter.defaultCenter().postNotificationName("barcodeScannerNotification", object: checkoutCode)
+                
+            })
+            
+        }
+    
+        self.output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        
+        for subview in self.view.subviews {
+            self.view.bringSubviewToFront(subview as! UIView)
+        }
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBarHidden = true
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        self.navigationController?.navigationBarHidden = false
+    }
+    
+}
--- FAU FabLab/Controllers/BarcodeScanner/ProductsearchCodeScannerViewController.swift
+++ FAU FabLab/Controllers/BarcodeScanner/ProductsearchCodeScannerViewController.swift
@@ -2,43 +2,26 @@ import UIKit
 import AVFoundation
 import RSBarcodes
 
-class BarcodeViewController: RSCodeReaderViewController {
+class ProductsearchCodeScannerViewController: RSCodeReaderViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        //Optional: Mark where code was found
         self.focusMarkLayer.strokeColor = UIColor.greenColor().CGColor
         self.cornersLayer.strokeColor = UIColor.greenColor().CGColor
         
         
-        //Barcode found, deal with it
         self.barcodesHandler = { barcodes in
             self.session.stopRunning()
             
-          
-            
-            //show UI Alert
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                
                 let productId = self.getProductIdFromBarcode(barcodes[0])
                 self.tabBarController?.selectedIndex = 2
                 NSNotificationCenter.defaultCenter().postNotificationName("barcodeScannerNotification", object: productId)
-   
             })
         
         }
-        
-        //VALID BARCODE TYPES:  (Bin mir nicht sicher welche wir alles bruachen...)
-        
-        //Ignore some
-        //let types = NSMutableArray(array: self.output.availableMetadataObjectTypes)
-        //types.removeObject(AVMetadataObjectTypeQRCode)
-        //self.output.metadataObjectTypes = NSArray(array: types) as [AnyObject]
-        
-        //OR  just add the needed once
-        self.output.metadataObjectTypes = [AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeEAN13Code,AVMetadataObjectTypeQRCode]
+        self.output.metadataObjectTypes = [AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeEAN13Code]
         
         
         for subview in self.view.subviews {
