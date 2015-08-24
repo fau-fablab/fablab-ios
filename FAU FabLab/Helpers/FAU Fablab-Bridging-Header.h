82ef3cc3a4e72a27839ae8764fe25e9b14d4c406
Katharina Full <katharina.full@fau.de>
2015-08-24 17:35:25 +0200

    Manually added Objective-C library and bridging header for badge bar button item

---
+++ FAU FabLab/Helpers/FAU Fablab-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  FAU Fablab-Bridging-Header.h
+//  FAU FabLab
+//
+//  Created by Katharina Full on 24.08.15.
+//  Copyright (c) 2015 FAU MAD FabLab. All rights reserved.
+//
+
+#ifndef FAU_FabLab_FAU_Fablab_Bridging_Header_h
+#define FAU_FabLab_FAU_Fablab_Bridging_Header_h
+
+#import "BBBadgeBarButtonItem.h"
+
+#endif
