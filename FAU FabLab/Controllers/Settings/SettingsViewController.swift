bf7aacfaac3a17bbea750e709c219cfceaa8ad6d
Emanuel Eimer <mail@eeimer.de>
2015-08-28 14:31:33 +0200

    Added switch for doorOpen-Push

--- FAU FabLab/Controllers/Settings/SettingsViewController.swift
+++ FAU FabLab/Controllers/Settings/SettingsViewController.swift
@@ -3,4 +3,11 @@ import Foundation
 
 class SettingsViewController : UIViewController{
 
+    
+    @IBOutlet weak var pushDoorSwitch: UISwitch!
+    
+    @IBAction func pushDoorSwitchChanged(sender: AnyObject) {
+        
+        
+    }
 }
