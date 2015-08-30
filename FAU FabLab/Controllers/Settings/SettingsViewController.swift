63f49bb30a103de4292cabca98032799c6af2930
Emanuel Eimer <mail@eeimer.de>
2015-08-30 16:39:47 +0200

    make use of new endpoint (get status if is registered to get push at door opens)

--- FAU FabLab/Controllers/Settings/SettingsViewController.swift
+++ FAU FabLab/Controllers/Settings/SettingsViewController.swift
@@ -7,35 +7,42 @@ class SettingsViewController : UIViewController{
     
     
     private var settings = Settings()
-    private let pushDoorOpensKey = "pushDoorOpens3"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if(settings.getValue(pushDoorOpensKey) != nil){
-            pushDoorSwitch.on = settings.getValue(pushDoorOpensKey)!
-        }else{
-            pushDoorSwitch.on = false
-        }
+       
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        // This is kind of a workround
+        //-> There is no garantie that push will be sent
+        //-> so the only save way is to ask the server about the status...
         
+        RestManager.sharedInstance.makeJsonGetRequest("/push/doorOpensNextTime", params: ["token": PushToken.token], onCompletion: {
+            json, err in
+            println(json)
+            println(json as! Bool)
+            self.pushDoorSwitch.on = json as! Bool
+            println(self.pushDoorSwitch.on)
+        })
     }
     
     
     
    
     @IBAction func pushDoorSwitchChanged(sender: AnyObject) {
-        settings.updateOrCreate(pushDoorOpensKey, value: pushDoorSwitch.on)
-        
+
         if(pushDoorSwitch.on){
             RestManager.sharedInstance.makeJsonPostRequest("/push/doorOpensNextTime", params: PushToken.serialize(), onCompletion: {
                 json, err in
-                println("ON")
             })
            
         }else{
             RestManager.sharedInstance.makeJsonPutRequest("/push/doorOpensNextTime", params: PushToken.serialize(), onCompletion: {
                 json, err in
-                println("OFF")
             })
         }
     }
