413d055a73c66459c4aba39d250da18b51453347
Max Jalowski <max.jalowski@fau.de>
2015-09-01 15:08:08 +0200

    New extension for doubles to calculate some values

---
+++ FAU FabLab/DoubleExtension.swift
@@ -0,0 +1,15 @@
+extension Double {
+    var digitsAfterComma: Int {
+        let s : String = String(stringInterpolationSegment: self)
+        let substring = s.substringFromIndex(find(s, ".")!.successor())
+        if substring == "0" {
+            return 0
+        }
+        return count(substring) as Int
+    }
+    
+    func roundDown(digitsAfterComma: Int) -> Double {
+        let p : Double = pow(10, Double(digitsAfterComma))
+        return Double(floor(p*self)/p)
+    }
+}
