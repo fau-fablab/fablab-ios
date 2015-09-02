3b51619e99f058e14feb88579a4ecf1762e7d791
Sebastian Haubner <sebastian.haubner@fau.de>
2015-09-02 22:46:34 +0200

    remove textfield and buttons in MalfunctionInfo

--- FAU FabLab/Controllers/MalfunctionInfo/MalfunctionInfoController.swift
+++ FAU FabLab/Controllers/MalfunctionInfo/MalfunctionInfoController.swift
@@ -2,62 +2,38 @@ import UIKit
 import MessageUI
 import CoreActionSheetPicker
 
-class MalfunctionInfoController: UIViewController, UITextViewDelegate, MFMailComposeViewControllerDelegate {
-    
-    @IBOutlet var buttonSelectMachine: UIButton!
-    @IBOutlet var affectedMachineLabel: UILabel!
-    @IBOutlet var textfield: UITextView!
-    @IBOutlet var buttonSendMail: UIButton!
-    @IBOutlet var textViewBottomConstraint: NSLayoutConstraint!
-    private var textViewBottomConstraintValue: CGFloat?
+class MalfunctionInfoController: UIViewController, MFMailComposeViewControllerDelegate {
     
     private let model = MalfunctionInfoModel()
-    private let placeholderText = "Bitte hier eine kurze Fehlerbeschreibung eintragen".localized
     private let cartButtonController = CartNavigationButtonController.sharedInstance
     
     var selectedMachine: String = ""{
         didSet{
-            self.affectedMachineLabel!.text = selectedMachine
-            if(errorMessage != placeholderText){
-                buttonSendMail.enabled = true
-            }else{
-                buttonSendMail.enabled = false
-            }
-        }
-    }
-    
-    var errorMessage: String = "Bitte hier eine kurze Fehlerbeschreibung eintragen".localized {
-        willSet(newText){
-            if(newText != placeholderText && selectedMachine != ""){
-                buttonSendMail.enabled = true
-            }else{
-                buttonSendMail.enabled = false
-            }
+            var picker = MFMailComposeViewController()
+            picker.mailComposeDelegate = self
+            picker.navigationBar.tintColor = UIColor.fabLabGreen()
+            picker.setToRecipients([model.fablabMail!])
+            picker.setSubject("Störungsmeldung".localized)
+            picker.setMessageBody(emailBody, isHTML: true)
+            
+            presentViewController(picker, animated: true, completion: nil)
         }
     }
     
     var emailBody: String{
-        return "<b>Tool:</b> </br> \(selectedMachine) </br></br> <b>Error Message:</b> </br> \(errorMessage) </br></br> " + "Gesendet mit der Fablab-iOS App".localized
+        return "<b>Tool:</b> </br> \(selectedMachine) </br></br> <b>Error Message:</b> </br>  </br></br> " + "Gesendet mit der Fablab-iOS App".localized
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        textViewBottomConstraintValue = self.textViewBottomConstraint.constant
-        textfield.delegate = self
-        textfield.text = placeholderText
-        textfield.textColor = UIColor.lightGrayColor()
+        model.fetchAllTools { () -> Void in
+            self.showPicker()
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         cartButtonController.setViewController(self)
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "changeTextFieldOnKeyboardChange:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "changeTextFieldOnKeyboardChange:", name: UIKeyboardWillHideNotification, object: nil)
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        NSNotificationCenter.defaultCenter().removeObserver(self);
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -67,46 +43,6 @@ class MalfunctionInfoController: UIViewController, UITextViewDelegate, MFMailCom
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func changeTextFieldOnKeyboardChange(notification: NSNotification) {
-        var info = notification.userInfo!
-        var keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
-        let duration = info[UIKeyboardAnimationDurationUserInfoKey] as! Double
-        
-        view.setNeedsUpdateConstraints()
-        
-        UIView.animateWithDuration(duration, animations: { () -> Void in
-            if notification.name == UIKeyboardWillShowNotification {
-                self.textViewBottomConstraint.constant = keyboardFrame.height + 10
-            }
-            else {
-                self.textViewBottomConstraint.constant = self.textViewBottomConstraintValue!
-            }
-        })
-    }
-    
-    func textViewDidBeginEditing(textView: UITextView) {
-        if textfield.text == placeholderText {
-            textfield.textColor = UIColor.blackColor()
-            textfield.text = ""
-        }
-    }
-    
-    func textViewDidEndEditing(textView: UITextView) {
-        if textfield.text.isEmpty{
-            textfield.textColor = UIColor.lightGrayColor()
-            textfield.text = placeholderText
-        }
-        errorMessage = textfield.text
-    }
-    
-    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
-        if text == "\n" {
-            textView.resignFirstResponder()
-            return false
-        }
-        return true
     }
     
     func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
@@ -120,10 +56,6 @@ class MalfunctionInfoController: UIViewController, UITextViewDelegate, MFMailCom
 
             case MFMailComposeResultSent.value:
                 Debug.instance.log("Sent!")
-                errorMessage = placeholderText
-                textfield.text = placeholderText
-                selectedMachine = " "
-                self.affectedMachineLabel!.text = selectedMachine
                 var alert = UIAlertController(title: "Versendet".localized, message: "Störungsmeldung wurde erfolgreich versendet!".localized, preferredStyle: UIAlertControllerStyle.Alert)
                 alert.addAction(UIAlertAction(title: "OK".localized, style: UIAlertActionStyle.Default, handler: nil))
                 self.presentViewController(alert, animated: true, completion: nil)
@@ -134,42 +66,23 @@ class MalfunctionInfoController: UIViewController, UITextViewDelegate, MFMailCom
         }
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
+    private func showPicker(){
+        var picker: ActionSheetStringPicker = ActionSheetStringPicker(title: "Betroffenes Gerät".localized, rows: self.model.getAllNames(), initialSelection: 0, doneBlock: {
+            picker, value, index in
+            self.selectedMachine = "\(index)"
+            return
+        }, cancelBlock: { ActionStringCancelBlock in return }, origin: nil)
         
-        return true
-    }
-    
-    @IBAction func buttonSelectMachineClicked(sender: AnyObject) {
-        model.fetchAllTools { () -> Void in
-            var picker: ActionSheetStringPicker = ActionSheetStringPicker(title: "Betroffenes Gerät".localized, rows: self.model.getAllNames(), initialSelection: 0, doneBlock: {
-                picker, value, index in
-                self.selectedMachine = "\(index)"
-                return
-                }, cancelBlock: { ActionStringCancelBlock in return }, origin: sender)
-            var doneButton: UIBarButtonItem = UIBarButtonItem()
-            doneButton.title = "Auswählen".localized
-            doneButton.tintColor = UIColor.fabLabGreen()            
-            var cancelButton: UIBarButtonItem = UIBarButtonItem()
-            cancelButton.title = "Abbrechen".localized
-            cancelButton.tintColor = UIColor.fabLabGreen()
+        var doneButton: UIBarButtonItem = UIBarButtonItem()
+        doneButton.title = "Auswählen".localized
+        doneButton.tintColor = UIColor.fabLabGreen()
+        var cancelButton: UIBarButtonItem = UIBarButtonItem()
+        cancelButton.title = "Abbrechen".localized
+        cancelButton.tintColor = UIColor.fabLabGreen()
             
-            picker.setDoneButton(doneButton)
-            picker.setCancelButton(cancelButton)
-            picker.tapDismissAction = TapAction.Cancel
-            picker.showActionSheetPicker()
-        }
+        picker.setDoneButton(doneButton)
+        picker.setCancelButton(cancelButton)
+        picker.tapDismissAction = TapAction.Cancel
+        picker.showActionSheetPicker()
     }
-    
-    @IBAction func buttonSendMailClicked(sender: AnyObject) {
-        var picker = MFMailComposeViewController()
-        picker.mailComposeDelegate = self
-        picker.navigationBar.tintColor = UIColor.fabLabGreen()
-        picker.setToRecipients([model.fablabMail!])
-        picker.setSubject("Störungsmeldung".localized)
-        picker.setMessageBody(emailBody, isHTML: true)
-        
-        presentViewController(picker, animated: true, completion: nil)
-    }
-    
 }
