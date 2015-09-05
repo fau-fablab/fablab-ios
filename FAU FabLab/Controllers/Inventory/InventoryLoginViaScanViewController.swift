4210058dc715406dc7b65cc77d9b73cc3afee432
Emanuel Eimer <mail@eeimer.de>
2015-09-06 01:09:25 +0200

    added scanner for login (via QR-Code) at inventory

---
+++ FAU FabLab/Controllers/Inventory/InventoryLoginViaScanViewController.swift
@@ -0,0 +1,51 @@
+import UIKit
+import AVFoundation
+import RSBarcodes
+
+class InventoryLoginViaScanViewController: RSCodeReaderViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.scan()
+    }
+    
+    private func scan(){
+        self.focusMarkLayer.strokeColor = UIColor.greenColor().CGColor
+        self.cornersLayer.strokeColor = UIColor.greenColor().CGColor
+        
+        self.barcodesHandler = { barcodes in
+            self.session.stopRunning()
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                let checkoutCode = barcodes[0].stringValue
+                self.dismissViewControllerAnimated(true, completion:nil )
+                
+                NSNotificationCenter.defaultCenter().postNotificationName("InventoryLoginNotification", object: checkoutCode)
+            })
+            
+        }
+        
+        self.output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        
+        for subview in self.view.subviews {
+            self.view.bringSubviewToFront(subview as! UIView)
+        }
+        
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBarHidden = true
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        self.navigationController?.navigationBarHidden = false
+    }
+    
+    @IBAction func cancelButtonTouched(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion:nil )
+    }
+}
