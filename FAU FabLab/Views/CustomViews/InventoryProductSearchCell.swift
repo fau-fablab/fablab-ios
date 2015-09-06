7e4a91a83e298aa62f1dfdad9c4d542a132a0f4a
Emanuel Eimer <mail@eeimer.de>
2015-09-07 00:49:06 +0200

    finished product search at inventory

--- FAU FabLab/Views/CustomViews/InventoryProductSearchCell.swift
+++ FAU FabLab/Views/CustomViews/InventoryProductSearchCell.swift
@@ -7,16 +7,9 @@ public class InventoryProductSearchCell: UITableViewCell {
     @IBOutlet var subtitle: UILabel!
     
     
-    private(set) var product:Product!
-    
 
     func configure(product: Product!) {
-        self.product = product
-        configure(product.name!, unit: product.unit!, price: product.price!)
-    }
-    
-    func configure(name: String, unit: String, price: Double) {
-        let separatedName = split(name, maxSplit: 1, allowEmptySlices: false, isSeparator: isSeparator)
+        let separatedName = split(product.name!, maxSplit: 1, allowEmptySlices: false, isSeparator: isSeparator)
         self.title.text = separatedName[0];
         if(separatedName.count > 1) {
             self.subtitle.text = separatedName[1];
