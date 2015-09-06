0e26fc6b445d1c519e1b94461cea35ed85ee92b5
Sebastian Haubner <sebastian.haubner@fau.de>
2015-09-07 00:05:26 +0200

    add InventoryApi

---
+++ FAU FabLab/Common/api/InventoryApi.swift
@@ -0,0 +1,20 @@
+import ObjectMapper
+
+struct InventoryApi{
+    
+    let api = RestManager.sharedInstance
+    let mapper = Mapper<InventoryItem>()
+    let resource = "/inventory"
+    
+    func getAll(onCompletion: ([InventoryItem]?, NSError?) -> Void){
+        api.makeJsonGetRequest(resource, params: nil, onCompletion: {
+            JSON, err in
+                if(err != nil){
+                    onCompletion(nil, err)
+                }
+                else{
+                    onCompletion(self.mapper.mapArray(JSON), nil)
+                }
+        })
+    }
+}
