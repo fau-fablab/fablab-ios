13883e2a1ad2aeb43245651cd8997dfc9a35946d
Sebastian Haubner <sebastian.haubner@fau.de>
2015-09-08 00:32:57 +0200

    finished InventoryApi

--- FAU FabLab/Common/api/InventoryApi.swift
+++ FAU FabLab/Common/api/InventoryApi.swift
@@ -3,7 +3,7 @@ import ObjectMapper
 struct InventoryApi{
     
     let api = RestManager.sharedInstance
-    let mapper = Mapper<InventoryItem>()
+    let inventoryMapper = Mapper<InventoryItem>()
     let resource = "/inventory"
     
     func getAll(onCompletion: ([InventoryItem]?, NSError?) -> Void){
@@ -12,7 +12,37 @@ struct InventoryApi{
                 if(err != nil){
                     onCompletion(nil, err)
                 }else{
-                    onCompletion(self.mapper.mapArray(JSON), nil)
+                    onCompletion(self.inventoryMapper.mapArray(JSON), nil)
+                }
+        })
+    }
+    
+    func add(user: User, item: InventoryItem, onCompletion: (InventoryItem?, NSError?) -> Void){
+        let params = inventoryMapper.toJSON(item)
+                
+        api.makeJSONRequestWithBasicAuth(.POST, encoding: .JSON, resource: resource, username: user.username!, password: user.password!, params: params,
+            onCompletion: {
+                JSON, err in
+                
+                if(err != nil){
+                    onCompletion(nil, err)
+                }
+                else{
+                    onCompletion(self.inventoryMapper.map(JSON), nil)
+                }
+        })
+    }
+    
+    func deleteAll(user: User, onCompletion: (Bool?, NSError?) -> Void){
+        api.makeJSONRequestWithBasicAuth(.DELETE, encoding: .URL, resource: resource, username: user.username!, password: user.password!, params: nil,
+            onCompletion: {
+                JSON, err in
+
+                if(err != nil){
+                    onCompletion(nil, err)
+                }
+                else{
+                    onCompletion(JSON as! Bool, nil)
                 }
         })
     }
