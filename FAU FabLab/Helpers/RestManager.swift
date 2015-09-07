bbdb0fb017baa652f25160e2074e79216c1c33eb
Sebastian Haubner <sebastian.haubner@fau.de>
2015-09-07 23:59:58 +0200

    restructure Restmanager and add missing header in requests

--- FAU FabLab/Helpers/RestManager.swift
+++ FAU FabLab/Helpers/RestManager.swift
@@ -6,102 +6,97 @@ typealias ServiceResponse = (String?, NSError?) -> Void
 typealias JsonServiceResponse = (AnyObject?, NSError?) -> Void
 
 class RestManager {
-    
-    static let sharedInstance = RestManager()
-    
-    private var manager:Manager;
-    
-    #if PRODUCTION
-        let apiUrl = "https://ec2-52-28-126-35.eu-central-1.compute.amazonaws.com:4433"
-    #else
-        let apiUrl = "https://ec2-52-28-163-255.eu-central-1.compute.amazonaws.com:4433"
-    #endif
-    
-    init(){
-        #if PRODUCTION
-            let serverTrustPolicies: [String: ServerTrustPolicy] = [
-                "ec2-52-28-126-35.eu-central-1.compute.amazonaws.com": .PinCertificates(
-                    certificates: ServerTrustPolicy.certificatesInBundle(),
-                    validateCertificateChain: true,
-                    validateHost: true
-                )
-            ]
-        #else
-            let serverTrustPolicies: [String: ServerTrustPolicy] = [
-                "ec2-52-28-163-255.eu-central-1.compute.amazonaws.com": .PinCertificates(
-                    certificates: ServerTrustPolicy.certificatesInBundle(),
-                    validateCertificateChain: true,
-                    validateHost: true
-                )
-            ]
-        #endif
-        println(apiUrl)
 
+    static let sharedInstance = RestManager()
+    private var manager: Manager;
+
+#if PRODUCTION
+    let apiUrl = "https://ec2-52-28-126-35.eu-central-1.compute.amazonaws.com:4433"
+    let serverTrustPolicies: [String:ServerTrustPolicy] = [
+            "ec2-52-28-126-35.eu-central-1.compute.amazonaws.com": .PinCertificates(
+            certificates: ServerTrustPolicy.certificatesInBundle(),
+                    validateCertificateChain: true,
+                    validateHost: true
+            )
+    ]
+#else
+    let apiUrl = "https://ec2-52-28-163-255.eu-central-1.compute.amazonaws.com:4433"
+    let serverTrustPolicies: [String:ServerTrustPolicy] = [
+            "ec2-52-28-163-255.eu-central-1.compute.amazonaws.com": .PinCertificates(
+            certificates: ServerTrustPolicy.certificatesInBundle(),
+                    validateCertificateChain: true,
+                    validateHost: true
+            )
+    ]
+#endif
+
+    init() {
         manager = Manager(
-            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
+        configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+                serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
         )
     }
-    
-    func makeJSONRequest(method: Alamofire.Method, encoding: ParameterEncoding, resource: String, params: [String : AnyObject]?, onCompletion : JsonServiceResponse) {
-        let endpoint = apiUrl+resource
-        manager.request(method, endpoint, parameters: params, encoding: encoding)
-            .validate(statusCode: 200..<300)
+
+    func makeJSONRequest(method: Alamofire.Method,
+                         encoding: ParameterEncoding,
+                         resource: String,
+                         params: [String:AnyObject]?,
+                         headers: [String:String]? = nil,
+                         onCompletion: JsonServiceResponse) {
+
+        let endpoint = apiUrl + resource
+        manager.request(method, endpoint, parameters: params, headers: headers, encoding: encoding)
+            .validate(statusCode: 200 ..< 300)
             .validate(contentType: ["application/json"])
             .responseJSON { (req, res, json, error) in
                 self.printDebug("GET", resource: endpoint, res: res, responseJson: json, error: error)
                 onCompletion(json, error);
         }
     }
-    
-    func makeTextRequest(method: Alamofire.Method, encoding: ParameterEncoding, resource: String, params: NSDictionary?, onCompletion : ServiceResponse) {
-        let endpoint = apiUrl+resource
-        manager.request(method, endpoint, parameters: params as? [String:AnyObject], encoding: encoding)
-            .validate(statusCode: 200..<300)
-            .validate(contentType: ["text/plain"])
-            .responseString { (req, res, answer, error) in
-                self.printDebug("GET", resource: endpoint, res: res, responseString: answer, error: error);
-                onCompletion(answer, error);
-        }
 
+    func makeTextRequest(method: Alamofire.Method,
+                         encoding: ParameterEncoding,
+                         resource: String,
+                         params: NSDictionary?,
+                         headers: [String:String]? = nil,
+                         onCompletion: ServiceResponse) {
+
+        let endpoint = apiUrl + resource
+        manager.request(method, endpoint, parameters: params as? [String:AnyObject], headers: headers, encoding: encoding)
+        .validate(statusCode: 200 ..< 300)
+        .validate(contentType: ["text/plain"])
+        .responseString { (req, res, answer, error) in
+            self.printDebug("GET", resource: endpoint, res: res, responseString: answer, error: error);
+            onCompletion(answer, error);
+        }
     }
-    
-    func makeJsonRequestWithBasicAuth(method: Alamofire.Method, resource: String, username: String, password: String, params: [String : AnyObject]?, onCompletion : JsonServiceResponse){
+
+    func makeJSONRequestWithBasicAuth(method: Alamofire.Method, encoding: ParameterEncoding, resource: String, username: String, password: String, params: [String:AnyObject]?, onCompletion: JsonServiceResponse) {
         let endpoint = apiUrl + resource
 
         let credentialData = "\(username):\(password)".dataUsingEncoding(NSUTF8StringEncoding)!
         let base64Credentials = credentialData.base64EncodedStringWithOptions(nil)
-        
-        let headers = ["Authorization": "Basic \(base64Credentials)"]
 
-        manager.request(method, endpoint, parameters: params, encoding: .URL, headers: headers)
-            .validate(statusCode: 200..<300)
-            .responseJSON{
-                request, response, JSON, err in
-                    if let err = err{
-                        let error = NSError(domain: "Auth", code: response!.statusCode, userInfo: nil)
-                        onCompletion(JSON, error)
-                    }
-                    onCompletion(JSON, nil)
-            }
+        let headers = ["Authorization": "Basic \(base64Credentials)", "Accept": "application/json"]
+
+        makeJSONRequest(method, encoding: encoding, resource: resource, params: params, headers: headers, onCompletion: onCompletion)
     }
-    
-    private func printDebug(method: String, resource: String, res: NSHTTPURLResponse?, responseJson: AnyObject?, error: NSError?){
-        if let res = res{
+
+    private func printDebug(method: String, resource: String, res: NSHTTPURLResponse?, responseJson: AnyObject?, error: NSError?) {
+        if let res = res {
             Debug.instance.log("\(method): \(resource) JSONAnswer: \(responseJson) StatusCode: \(res.statusCode)");
         }
-        if let error = error{
+        if let error = error {
             Debug.instance.log(error)
         }
     }
-    
-    private func printDebug(method: String, resource: String, res: NSHTTPURLResponse?, responseString: String?, error: NSError?){
-        if let res = res{
+
+    private func printDebug(method: String, resource: String, res: NSHTTPURLResponse?, responseString: String?, error: NSError?) {
+        if let res = res {
             Debug.instance.log("\(method): \(resource) Answer: \(responseString) StatusCode: \(res.statusCode)");
         }
-        if let error = error{
+        if let error = error {
             Debug.instance.log(error)
         }
     }
-    
 }
