f527a87ae2710a9c163550e1d75abc04f5b88d2d
Emanuel Eimer <mail@eeimer.de>
2015-09-14 17:47:12 +0200

    added prefix for checkout-code

--- FAU FabLab/Controllers/BarcodeScanner/CheckoutCodeScannerViewController.swift
+++ FAU FabLab/Controllers/BarcodeScanner/CheckoutCodeScannerViewController.swift
@@ -21,9 +21,17 @@ class CheckoutCodeScannerViewController: RSCodeReaderViewController {
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
                 
                 let checkoutCode = barcodes[0].stringValue
-                self.dismissViewControllerAnimated(true, completion:nil )
-                
-                NSNotificationCenter.defaultCenter().postNotificationName("CheckoutScannerNotification", object: checkoutCode)
+                let prefix = (checkoutCode as NSString).substringToIndex(3)
+                if prefix == "FAU" {
+                    self.dismissViewControllerAnimated(true, completion:nil )
+                    NSNotificationCenter.defaultCenter().postNotificationName("CheckoutScannerNotification", object: checkoutCode)
+                }else{
+                    var alert = UIAlertController(title: "Fehler".localized, message: "Kein gültiger Code!".localized, preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "OK".localized, style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    self.session.startRunning()
+                }
+              
                 
             })
             
