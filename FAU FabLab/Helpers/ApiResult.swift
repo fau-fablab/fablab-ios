2a54dd2ce47aa4219d86a2ebecbf6e3e41d8a427
Sebastian Haubner <sebastian.haubner@fau.de>
2015-09-15 15:01:20 +0200

    add ApiResult as a helper class for easier response mapping to objects

---
+++ FAU FabLab/Helpers/ApiResult.swift
@@ -0,0 +1,23 @@
+import ObjectMapper
+struct ApiResult{
+    
+    static func get<T: Mappable>(data: AnyObject?, error: NSError?, completionHandler: (T?, NSError?) -> Void){
+        if(error != nil){
+            completionHandler(nil, error)
+        }
+        else{
+            let result = Mapper<T>().map(data)
+            completionHandler(result, nil)
+        }
+    }
+    
+    static func getArray<T: Mappable>(data: AnyObject?, error: NSError?, completionHandler: ([T]?, NSError?) -> Void){
+        if(error != nil){
+            completionHandler(nil, error)
+        }
+        else{
+            let result = Mapper<T>().mapArray(data)
+            completionHandler(result, nil)
+        }
+    }
+}
