2d4bfef7fa258ce64067c7dc3a77bdf0abb5d098
Sebastian Haubner <sebastian.haubner@fau.de>
2015-09-15 16:53:37 +0200

    add NewsApi

---
+++ FAU FabLab/Common/api/NewsApi.swift
@@ -0,0 +1,56 @@
+struct NewsApi {
+    
+    let resource = "/news"
+    
+    func findById(id: Int64, onCompletion: (News?, NSError?) -> Void){
+        let endpoint = resource + "/\(id)"
+        
+        RestManager.sharedInstance.makeJSONRequest(.GET, encoding: .URL, resource: endpoint, params: nil,
+            onCompletion: { json, err in
+                ApiResult.get(json, error: err, completionHandler: onCompletion)
+        })
+    }
+    
+    func find(offset: Int, limit: Int, onCompletion: ([News]?, NSError?) -> Void){
+        let params = ["offset": offset, "limit" : limit]
+        
+        RestManager.sharedInstance.makeJSONRequest(.GET, encoding: .URL, resource: resource, params: params,
+            onCompletion: { json, err in
+            ApiResult.getArray(json, error: err, completionHandler: onCompletion)
+        })
+    }
+    
+    func findAll(onCompletion: ([News]?, NSError?) -> Void){
+        let endpoint = resource + "/all"
+        
+        RestManager.sharedInstance.makeJSONRequest(.GET, encoding: .URL, resource: endpoint, params: nil,
+            onCompletion: { json, err in
+                ApiResult.getArray(json, error: err, completionHandler: onCompletion)
+        })
+    }
+    
+    func findNewsSince(timestamp: Int64, onCompletion: ([News]?, NSError?) -> Void){
+        let endpoint = resource + "/all/\(timestamp)"
+        
+        RestManager.sharedInstance.makeJSONRequest(.GET, encoding: .URL, resource: endpoint, params: nil,
+            onCompletion: {   json, err in
+                ApiResult.getArray(json, error: err, completionHandler: onCompletion)
+        })
+    }
+    
+    func lastUpdate(onCompletion: (Int64?, NSError?) -> Void){
+        let endpoint = resource + "/timestamp"
+        
+        RestManager.sharedInstance.makeJSONRequest(.GET, encoding: .URL, resource: endpoint, params: nil,
+            onCompletion: { json, err in
+                if(err != nil){
+                    onCompletion(-1, err)
+                }
+                else{
+                    var num = json as? NSNumber
+                    var result = num?.longLongValue
+                    onCompletion(result, nil)
+                }
+        })
+    }
+}
