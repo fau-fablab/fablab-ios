26a61d5a08d719cc75b2e82ffaa0a42b800262ac
Emanuel Eimer <mail@eeimer.de>
2015-09-15 03:35:48 +0200

    implemented tableView at checkoutView -> Shows Cart-Items now during "wait for payment"

--- FAU FabLab/Controllers/Cart/PayOrCancelViewController.swift
+++ FAU FabLab/Controllers/Cart/PayOrCancelViewController.swift
@@ -1,13 +1,53 @@
 import Foundation
 import UIKit
 
-class PayOrCancelViewController : UIViewController{
-    
-    
+class PayOrCancelViewController : UIViewController, UITableViewDataSource, UITableViewDelegate, UITextViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var cancelButton: UIButton!
-     override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    private var tableViewCellIdentifier = "PayOrCancelCell"
+    private var cartModel = CartModel()
+    
+    override func viewDidLoad() {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "checkoutStatusChanged:", name: "CheckoutStatusChangedNotification", object: nil)
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        spinner.startAnimating()
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cartModel.getNumberOfProductsInCart()
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(tableViewCellIdentifier, forIndexPath: indexPath) as! PayOrCancelCell
+        cell.configure(cartModel.getProductInCart(indexPath.row))
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Produkte".localized
+    }
+    
+    func setCartModel(cartModel : CartModel){
+        self.cartModel = cartModel
     }
     
     func checkoutStatusChanged(notification:NSNotification) {
@@ -18,6 +58,8 @@ class PayOrCancelViewController : UIViewController{
         cancelButton.enabled = false
         CartModel.sharedInstance.cancelCheckoutProcessByUser()
     }
-
     
 }
+
+
+
