54d8f8b3b7146b60fd803c5b53ae37dd59a1da6a
Katharina Full <katharina.full@fau.de>
2015-09-21 11:12:44 +0200

    Added custom cell for tool usage

---
+++ FAU FabLab/Views/CustomViews/ToolUsageCustomCell.swift
@@ -0,0 +1,28 @@
+import Foundation
+import UIKit
+
+
+class ToolUsageCustomCell: UITableViewCell {
+    
+    @IBOutlet var user: UILabel!
+    @IBOutlet var project: UILabel!
+    @IBOutlet var duration: UILabel!
+    @IBOutlet var time: UILabel!
+    
+    func configure(toolUsage: ToolUsage, startingTime: Int64) {
+        
+        user.text = toolUsage.user
+        project.text = toolUsage.project
+        duration.text = "\(toolUsage.duration!) " + "Minuten".localized
+        
+        let timeInterval: NSTimeInterval = Double(startingTime)
+        let date = NSDate(timeIntervalSince1970: timeInterval)
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.NoStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        time.text = "ab".localized + " \(dateFormatter.stringFromDate(date)) " + "Uhr".localized
+
+    }
+    
+}
